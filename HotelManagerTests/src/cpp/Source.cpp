f625c6278bbdc20c28f5969d8ae9b42de731e64f
rekomo <rekomo.code@protonmail.com>
2023-12-22 23:59:43 +0100

    added split string util

--- HotelManagerTests/src/cpp/Source.cpp
+++ HotelManagerTests/src/cpp/Source.cpp
@@ -11,12 +11,44 @@ namespace UnitTestTest
 	TEST_CLASS(UnitTestTest)
 	{
 	public:
+		TEST_METHOD(Test) {
+			Logger::WriteMessage("Test log");
+			Assert::IsTrue(true);
+		}
 
 		TEST_METHOD(StringSplit)
 		{
 			auto sp = hotel_klasowy::Utils::split("a;b;c", ";");
 			std::vector<std::string> expected = { "a", "b", "c" };
-			Logger::WriteMessage(fmt::format("{} -> {}", sp.size(), sp[0]).c_str());
+			Logger::WriteMessage(fmt::format("Size: {}\n", sp.size()).c_str());
+			for (auto& s : sp) {
+				Logger::WriteMessage(fmt::format("{}\n",s).c_str());
+			}
+			Assert::AreEqual(expected.size(), sp.size());
+			Assert::IsTrue(expected == sp);
+		}
+
+		TEST_METHOD(StringSplitLonger)
+		{
+			auto sp = hotel_klasowy::Utils::split("Anna;;-Maciek a;;- Franek;", ";;-");
+			std::vector<std::string> expected = { "Anna", "Maciek a", " Franek;" };
+			Logger::WriteMessage(fmt::format("Size: {}\n", sp.size()).c_str());
+			for (auto& s : sp) {
+				Logger::WriteMessage(fmt::format("{}\n", s).c_str());
+			}
+			Assert::AreEqual(expected.size(), sp.size());
+			Assert::IsTrue(expected == sp);
+		}
+
+		TEST_METHOD(StringSplitNotSplit)
+		{
+			auto sp = hotel_klasowy::Utils::split("Ola ma kota", "%");
+			std::vector<std::string> expected = { "Ola ma kota" };
+			Logger::WriteMessage(fmt::format("Size: {}\n", sp.size()).c_str());
+			for (auto& s : sp) {
+				Logger::WriteMessage(fmt::format("{}\n", s).c_str());
+			}
+			Assert::AreEqual(expected.size(), sp.size());
 			Assert::IsTrue(expected == sp);
 		}
 	};
