358829a13c25813f04fd8ac54bd8729e387cff66
Lukasz <lukasz.wilk231@gmail.com>
2023-12-28 16:38:05 +0100

    interfejs sprzataczki dziala poprawnie

--- HotelManager/src/cpp/ObslugaSprzataczki.cpp
+++ HotelManager/src/cpp/ObslugaSprzataczki.cpp
@@ -26,13 +26,54 @@ void hotel_klasowy::ObslugaSprzataczki::interfejs(Osoba osoba) {
 	cout << "-                                                                                                                -" << endl;
 	cout << "-                                                                                                                -" << endl;
 	cout << "------------------------------------------------------------------------------------------------------------------" << endl;
+	int opcja = 0;
+	cin >> opcja;
+
+	while (opcja != 1 && opcja != 2) {
+		system("cls");
+		cout << "------------------------------------------------------------------------------------------------------------------" << endl;
+		cout << "-                                                                                                    " << osoba.imie << " " << osoba.nazwisko << " - " << endl;
+		cout << "-                                    Jestes zalogowany/a jako Sprzatacz/ka                                     " << osoba.id << " -" << endl;
+		cout << "-                                                                                                                -" << endl;
+		cout << "-                                                                                                                -" << endl;
+		cout << "-                                                   OPCJE                                                        -" << endl;
+		cout << "-                                                                                                                -" << endl;
+		cout << "-                                  ========================================                                      -" << endl;
+		cout << "-                                  |                                      |                                      -" << endl;
+		cout << "-                                  |  1. Wyswietl pokoje do posprzatania  |                                      -" << endl;
+		cout << "-                                  |                                      |                                      -" << endl;
+		cout << "-                                  |   2. Oznacz pokoj jako posprzatany   |                                      -" << endl;
+		cout << "-                                  |                                      |                                      -" << endl;
+		cout << "-                                  ========================================                                      -" << endl;
+		cout << "-                                                                                                                -" << endl;
+		cout << "-                                                                                                                -" << endl;
+		cout << "-                                                                                                                -" << endl;
+		cout << "-                                                                                                                -" << endl;
+		cout << "------------------------------------------------------------------------------------------------------------------" << endl;
+		cout << "Nie ma takiej operacji!" << endl;
+		cin >> opcja;
+	}
+
+	switch (opcja) {
+	case 1:
+		wyswietlPokojeDoPosprzatania();
+		break;
+	case 2:
+		oznaczPokojJakoPosprzatany(5);
+		break;
+	default:
+		cout << "Cos poszlo nie tak" << endl;
+	}
 }
 
 void hotel_klasowy::ObslugaSprzataczki::wyswietlPokojeDoPosprzatania() {
-	throw "Not yet implemented";
+	system("cls");
+	cout << "Lista pokoi do posprzatania" << endl;
 }
 
 bool hotel_klasowy::ObslugaSprzataczki::oznaczPokojJakoPosprzatany(int idPokoju) {
-	throw "Not yet implemented";
+	system("cls");
+	cout << "Pokoj posprzatany" << endl;
+	return true;
 }
 
