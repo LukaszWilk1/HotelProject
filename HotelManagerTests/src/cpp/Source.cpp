c042f650351ae34707ec8e8dd538afebbbeef788
Agata <agata.wojciechowska39@o2.pl>
2024-01-04 09:59:35 +0100

    test BazaUzytkownikow

--- HotelManagerTests/src/cpp/Source.cpp
+++ HotelManagerTests/src/cpp/Source.cpp
@@ -2,6 +2,8 @@
 #include "CppUnitTest.h"
 #include "Utils.cpp"
 #include "fmt/core.h"
+#include "BazaUzytkownikow.cpp"
+#include "TypyKont.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -73,6 +75,18 @@ namespace UnitTestTest
 			}
 			Assert::AreEqual(expected.size(), sp.size());
 			Assert::IsTrue(expected == sp);
+		}
+
+		TEST_METHOD(ZapisywanieBazyUzytkownikow)
+		{
+			hotel_klasowy::BazaUzytkownikow b;
+			int id = b.dodajUzytkownika("Maria", "Wojtas", hotel_klasowy::KLIENT);
+			hotel_klasowy::BazaUzytkownikow b2;
+			auto u = b2.getUzytkownik(id);
+			Assert::IsTrue(u.id == id);
+			Assert::IsTrue(u.imie == "Maria");
+			Assert::IsTrue(u.nazwisko == "Wojtas");
+			Assert::IsTrue(u.typ_konta == hotel_klasowy::KLIENT);
 		}
 	};
 }
