ab8e7bccf8fe73e6738724e7102dc761295a1797
Agata <agata.wojciechowska39@o2.pl>
2024-01-06 22:13:36 +0100

    test czy daty sa rowne

--- HotelManagerTests/src/cpp/Source.cpp
+++ HotelManagerTests/src/cpp/Source.cpp
@@ -129,5 +129,12 @@ namespace UnitTestTest
 			Assert::AreEqual(1, d.getDzien()); 
 		}
 
+		TEST_METHOD(TestRoznicaMiedzyDatami)
+		{
+			hotel_klasowy::Data d_1(6, 1, 2024);
+			hotel_klasowy::Data d_2(9, 1, 2024);
+			Assert::IsTrue(d_1 == d_2);
+		}
+
 	};
 }
