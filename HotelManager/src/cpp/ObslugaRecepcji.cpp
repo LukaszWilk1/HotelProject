e0f0678031b3fa7179d122fe916d620a2a6be657
Lukasz <lukasz.wilk231@gmail.com>
2024-01-09 18:28:32 +0100

    Zapetlono obsluge recepcji

--- HotelManager/src/cpp/ObslugaRecepcji.cpp
+++ HotelManager/src/cpp/ObslugaRecepcji.cpp
@@ -61,47 +61,8 @@ void hotel_klasowy::ObslugaRecepcji::odblokujPokoj(int idPokoju) {
 }
 
 void hotel_klasowy::ObslugaRecepcji::interfejs() {
-	system("cls");
-	cout << "------------------------------------------------------------------------------------------------------------------" << endl;
-	cout << "-                                                                                                    " << osoba.imie << " " << osoba.nazwisko << " - " << endl;
-	cout << "-                                  Jestes zalogowany/a jako Recepcjonista/ka                                   " << osoba.id << " -" << endl;
-	cout << "-                                                                                                                -" << endl;
-	cout << "-                                                   OPCJE                                                        -" << endl;
-	cout << "-                                                                                                                -" << endl;
-	cout << "-                                  ========================================                                      -" << endl;
-	cout << "-                                  |                                      |                                      -" << endl;
-	cout << "-                                  |         1. Dodaj Recerwacje          |                                      -" << endl;
-	cout << "-                                  |                                      |                                      -" << endl;
-	cout << "-                                  |   2. Wyswietl wszystkie rezerwacje   |                                      -" << endl;
-	cout << "-                                  |                                      |                                      -" << endl;
-	cout << "-                                  |         3. Anuluj rezerwacje         |                                      -" << endl;
-	cout << "-                                  |                                      |                                      -" << endl;
-	cout << "-                                  |        4. Wyswietl rezerwacje        |                                      -" << endl;
-	cout << "-                                  |                                      |                                      -" << endl;
-	cout << "-                                  |      5. Zmien termin rezerwacji      |                                      -" << endl;
-	cout << "-                                  |                                      |                                      -" << endl;
-	cout << "-                                  |         6. Oplac rezerwacje          |                                      -" << endl;
-	cout << "-                                  |                                      |                                      -" << endl;
-	cout << "-                                  |          7. Wyswietl pokoj           |                                      -" << endl;
-	cout << "-                                  |                                      |                                      -" << endl;
-	cout << "-                                  |     8. Wyswietl wszystkie pokoje     |                                      -" << endl;
-	cout << "-                                  |                                      |                                      -" << endl;
-	cout << "-                                  |          9. Zablokoj pokoj           |                                      -" << endl;
-	cout << "-                                  |                                      |                                      -" << endl;
-	cout << "-                                  |           10. Zakwateruj             |                                      -" << endl;
-	cout << "-                                  |                                      |                                      -" << endl;
-	cout << "-                                  |           11. Wykwateruj             |                                      -" << endl;
-	cout << "-                                  |                                      |                                      -" << endl;
-	cout << "-                                  |         12. Odblokuj pokoj           |                                      -" << endl;
-	cout << "-                                  |                                      |                                      -" << endl;
-	cout << "-                                  ========================================                                      -" << endl;
-	cout << "-                                                                                                                -" << endl;
-	cout << "------------------------------------------------------------------------------------------------------------------" << endl;
-
-	int opcja = 0;
-	cin >> opcja;
-
-	while (opcja != 1 && opcja != 2 && opcja != 3 && opcja != 4 && opcja != 5 && opcja != 6 && opcja != 7 && opcja != 8 && opcja != 9 && opcja != 10 && opcja != 11 && opcja != 12) {
+	string exit;
+	do {
 		system("cls");
 		cout << "------------------------------------------------------------------------------------------------------------------" << endl;
 		cout << "-                                                                                                    " << osoba.imie << " " << osoba.nazwisko << " - " << endl;
@@ -138,49 +99,114 @@ void hotel_klasowy::ObslugaRecepcji::interfejs() {
 		cout << "-                                  ========================================                                      -" << endl;
 		cout << "-                                                                                                                -" << endl;
 		cout << "------------------------------------------------------------------------------------------------------------------" << endl;
-		cout << "Nie ma takiej operacji!" << endl;
+
+		int opcja = 0;
 		cin >> opcja;
-	}
 
-	switch (opcja) {
-	case 1:
-		dodajRezerwacje();
-		break;
-	case 2:
-		wyswietlWszstkieRezerwacje();
-		break;
-	case 3:
-		anulujRezerwacje(4);
-		break;
-	case 4:
-		wyswietlRezerwacje(4);
-		break;
-	case 5:
-		zmienTerminRezerwacji(4);
-		break;
-	case 6:
-		oplacRezerwacje(4);
-		break;
-	case 7:
-		wyswietlPokoj(4);
-		break;
-	case 8:
-		wyswietlWszystkiePokoje();
-		break;
-	case 9:
-		zablokujPokoj(2);
-		break;
-	case 10:
-		zakwateruj(4);
-		break;
-	case 11:
-		wykwateruj(4);
-		break;
-	case 12:
-		odblokujPokoj(2);
-		break;
-	}
+		while (opcja != 1 && opcja != 2 && opcja != 3 && opcja != 4 && opcja != 5 && opcja != 6 && opcja != 7 && opcja != 8 && opcja != 9 && opcja != 10 && opcja != 11 && opcja != 12) {
+			system("cls");
+			cout << "------------------------------------------------------------------------------------------------------------------" << endl;
+			cout << "-                                                                                                    " << osoba.imie << " " << osoba.nazwisko << " - " << endl;
+			cout << "-                                  Jestes zalogowany/a jako Recepcjonista/ka                                   " << osoba.id << " -" << endl;
+			cout << "-                                                                                                                -" << endl;
+			cout << "-                                                   OPCJE                                                        -" << endl;
+			cout << "-                                                                                                                -" << endl;
+			cout << "-                                  ========================================                                      -" << endl;
+			cout << "-                                  |                                      |                                      -" << endl;
+			cout << "-                                  |         1. Dodaj Recerwacje          |                                      -" << endl;
+			cout << "-                                  |                                      |                                      -" << endl;
+			cout << "-                                  |   2. Wyswietl wszystkie rezerwacje   |                                      -" << endl;
+			cout << "-                                  |                                      |                                      -" << endl;
+			cout << "-                                  |         3. Anuluj rezerwacje         |                                      -" << endl;
+			cout << "-                                  |                                      |                                      -" << endl;
+			cout << "-                                  |        4. Wyswietl rezerwacje        |                                      -" << endl;
+			cout << "-                                  |                                      |                                      -" << endl;
+			cout << "-                                  |      5. Zmien termin rezerwacji      |                                      -" << endl;
+			cout << "-                                  |                                      |                                      -" << endl;
+			cout << "-                                  |         6. Oplac rezerwacje          |                                      -" << endl;
+			cout << "-                                  |                                      |                                      -" << endl;
+			cout << "-                                  |          7. Wyswietl pokoj           |                                      -" << endl;
+			cout << "-                                  |                                      |                                      -" << endl;
+			cout << "-                                  |     8. Wyswietl wszystkie pokoje     |                                      -" << endl;
+			cout << "-                                  |                                      |                                      -" << endl;
+			cout << "-                                  |          9. Zablokoj pokoj           |                                      -" << endl;
+			cout << "-                                  |                                      |                                      -" << endl;
+			cout << "-                                  |           10. Zakwateruj             |                                      -" << endl;
+			cout << "-                                  |                                      |                                      -" << endl;
+			cout << "-                                  |           11. Wykwateruj             |                                      -" << endl;
+			cout << "-                                  |                                      |                                      -" << endl;
+			cout << "-                                  |         12. Odblokuj pokoj           |                                      -" << endl;
+			cout << "-                                  |                                      |                                      -" << endl;
+			cout << "-                                  ========================================                                      -" << endl;
+			cout << "-                                                                                                                -" << endl;
+			cout << "------------------------------------------------------------------------------------------------------------------" << endl;
+			cout << "Nie ma takiej operacji!" << endl;
+			cin >> opcja;
+		}
 
+		switch (opcja) {
+		case 1:
+			dodajRezerwacje();
+			cout << "Wpisz q aby wyjsc lub cokolwiek innego aby wrocic do listy opcji: ";
+			cin >> exit;
+			break;
+		case 2:
+			wyswietlWszstkieRezerwacje();
+			cout << "Wpisz q aby wyjsc lub cokolwiek innego aby wrocic do listy opcji: ";
+			cin >> exit;
+			break;
+		case 3:
+			anulujRezerwacje(4);
+			cout << "Wpisz q aby wyjsc lub cokolwiek innego aby wrocic do listy opcji: ";
+			cin >> exit;
+			break;
+		case 4:
+			wyswietlRezerwacje(4);
+			cout << "Wpisz q aby wyjsc lub cokolwiek innego aby wrocic do listy opcji: ";
+			cin >> exit;
+			break;
+		case 5:
+			zmienTerminRezerwacji(4);
+			cout << "Wpisz q aby wyjsc lub cokolwiek innego aby wrocic do listy opcji: ";
+			cin >> exit;
+			break;
+		case 6:
+			oplacRezerwacje(4);
+			cout << "Wpisz q aby wyjsc lub cokolwiek innego aby wrocic do listy opcji: ";
+			cin >> exit;
+			break;
+		case 7:
+			wyswietlPokoj(4);
+			cout << "Wpisz q aby wyjsc lub cokolwiek innego aby wrocic do listy opcji: ";
+			cin >> exit;
+			break;
+		case 8:
+			wyswietlWszystkiePokoje();
+			cout << "Wpisz q aby wyjsc lub cokolwiek innego aby wrocic do listy opcji: ";
+			cin >> exit;
+			break;
+		case 9:
+			zablokujPokoj(2);
+			cout << "Wpisz q aby wyjsc lub cokolwiek innego aby wrocic do listy opcji: ";
+			cin >> exit;
+			break;
+		case 10:
+			zakwateruj(4);
+			cout << "Wpisz q aby wyjsc lub cokolwiek innego aby wrocic do listy opcji: ";
+			cin >> exit;
+			break;
+		case 11:
+			wykwateruj(4);
+			cout << "Wpisz q aby wyjsc lub cokolwiek innego aby wrocic do listy opcji: ";
+			cin >> exit;
+			break;
+		case 12:
+			odblokujPokoj(2);
+			cout << "Wpisz q aby wyjsc lub cokolwiek innego aby wrocic do listy opcji: ";
+			cin >> exit;
+			break;
+		}
+	} while (exit != "Q" && exit != "q");
 }
 
 void hotel_klasowy::ObslugaRecepcji::dodajRezerwacje() {
